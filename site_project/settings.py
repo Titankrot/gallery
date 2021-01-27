1a65bd505782f5bc788a129b014f7b25a2b52e31
Titankrot <50486923+Titankrot@users.noreply.github.com>
2021-01-28 01:53:56 +0500

    assaaaaaaaaaa

--- site_project/settings.py
+++ site_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 import django_heroku
 
 
@@ -89,8 +88,6 @@ DATABASES = {
         'NAME': 'mydatabase',
     }
 }
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
